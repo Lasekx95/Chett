fa2ef4120dc659b2bf83d6151348a50ff17418c0
Dustin <ddemers1995@hotmail.com>
2023-11-24 08:56:08 -0800

    created ejs files, server, and begun design

--- app.js
+++ app.js
@@ -3,12 +3,28 @@ import express from "express";
 const app = express();
 const port = 4050;
 
+// view engine setup
+app.use(expressLayouts)
 app.set('view engine', 'ejs');
+app.set('views', 'views')
+app.set('layout', 'layout/main')
 
 app.get('/', (req, res) => {
-    res.send('Welcome to my server!');
+    res.render('home'); 
+  });
+
+  app.get('/accounts', (req, res) => {
+    res.render('accounts'); 
+  });
+
+  app.get('/pricing', (req, res) => {
+    res.render('pricing'); 
+  });
+
+  app.get('/data', (req, res) => {
+    res.render('data'); 
   });
   
-  app.listen(port, () => {
+app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
-  });
+});

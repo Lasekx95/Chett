b8e9a527d107b6bbc441bcb07197c16eeef668ce
Dustin <ddemers1995@hotmail.com>
2023-11-29 15:04:50 -0800

    added comments to app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const app = express()
 const port = 4050;
 
-const Product = require('./models/product');
+const Product = require('./models/product'); //importing productSchema from models folder
 
 //middleware
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -18,70 +18,70 @@ app.set('views', 'views')
 app.set('layout', 'layout/main')
 
 //product controllers
-app.post('/productADD', async (req, res) => {
+app.post('/productADD', async (req, res) => { //post request to add product
   try {
-    const { ProductName, priceUSD, priceCAD } = req.body;
+    const { ProductName, priceUSD, priceCAD } = req.body; //destructure the request body
 
-    const newProduct = new Product({
+    const newProduct = new Product({ //create a new product
       ProductName,
       priceUSD,
       priceCAD,
     });
 
-    await newProduct.save();
+    await newProduct.save(); //save the product to the database
 
-    res.redirect('/products');
+    res.redirect('/products'); //redirect to the products page
   } catch (error) {
     console.error(error);
     res.status(500).send('Internal Server Error');
   }
 });
 
-app.get('/products', async (req, res) => {
+app.get('/products', async (req, res) => { //get request to display products
   try {
     const products = await Product.find();
-    res.render('products', { products });
+    res.render('products', { products }); //render the products page
   } catch (error) {
     console.error(error);
     res.status(500).send('Internal Server Error');
   }
 });
 
-app.get('/editProduct/:id', async (req, res) => {
+app.get('/editProduct/:id', async (req, res) => { //get request to edit product by id
   try {
-    const productId = req.params.id;
-    const product = await Product.findById(productId);
-    res.render('editProduct', { product });
+    const productId = req.params.id; //get the product id from the request parameters
+    const product = await Product.findById(productId); //find the product by id
+    res.render('editProduct', { product }); //render the edit product page
   } catch (error) {
     console.error(error);
     res.status(500).send('Internal Server Error');
   }
 });
 
-app.post('/editProduct/:id', async (req, res) => {
+app.post('/editProduct/:id', async (req, res) => { //post request to edit product by id 
   try {
-    const productId = req.params.id;
-    const { productName, priceUSD, priceCAD } = req.body;
+    const productId = req.params.id; //get the product id from the request parameters
+    const { productName, priceUSD, priceCAD } = req.body; //destructure the request body
 
     // Update the product in the database
-    await Product.findByIdAndUpdate(productId, {
+    await Product.findByIdAndUpdate(productId, { //find the product by id and update it
       ProductName: productName,
       priceUSD: priceUSD,
       priceCAD: priceCAD,
     });
 
-    res.redirect('/products');
+    res.redirect('/products'); //redirect to the products page
   } catch (error) {
     console.error(error);
     res.status(500).send('Internal Server Error');
   }
 });
 
-app.get('/deleteProduct/:id', async (req, res) => {
+app.get('/deleteProduct/:id', async (req, res) => { //get request to delete product by id
   try {
     const productId = req.params.id;
-    await Product.findByIdAndRemove(productId);
-    res.redirect('/products');
+    await Product.findByIdAndRemove(productId); //find the product by id and remove it
+    res.redirect('/products'); //redirect to the products page
   } catch (error) {
     console.error(error);
     res.status(500).send('Internal Server Error');
